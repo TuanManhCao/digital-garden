0be9ae3ff3d0b0cdb46e30dbdebee659e7b9247c
Tuan Cao <caomanhtuanbsb@gmail.com>
2022-03-23 11:14:40 +0700

    Update project to Next v12

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-    webpack: (config, { isServer }) => {
-        // Fixes npm packages that depend on `fs` module
-        if (!isServer) {
-            config.node = {
-                fs: 'empty'
-            }
-        }
+    swcMinify: true,
+    webpack: (config) => {
+        config.resolve.fallback = {
+            fs: false,
+            buffer: false
+        };
 
-        return config
-    }
+        return config;
+    },
 }
