e331a9c7c93ae56c0f6eca819b8c2531c8c29c3f
Tuan Cao <caomanhtuanbsb@gmail.com>
2022-04-18 16:47:02 +0700

    Filter out non-existent target edge

--- pages/note/[id].js
+++ pages/note/[id].js
@@ -10,7 +10,6 @@ import {
 import FolderTree from "../../components/FolderTree";
 import {getFlattenArray} from "../../lib/utils";
 import MDContent from "../../components/MDContent";
-// import
 import dynamic from 'next/dynamic'
 
 const DynamicGraph = dynamic(
@@ -19,16 +18,6 @@ const DynamicGraph = dynamic(
 )
 
 export default function Home({note, backLinks, fileNames, tree, flattenNodes, graphData}) {
-
-    // const router = useRouter();
-    //
-    // useEffect(() => {
-    //     if (isMenuOpen) {
-    //         setMenuOpen(!isMenuOpen);
-    //     }
-    // }, [router.asPath]);
-    //
-
 
     return (
         <Layout>
@@ -40,7 +29,7 @@ export default function Home({note, backLinks, fileNames, tree, flattenNodes, gr
                     <FolderTree tree={tree} flattenNodes={flattenNodes}/>
                 </nav>
                 <MDContent content={note.data} fileNames={fileNames} handleOpenNewContent={null} backLinks={backLinks}/>
-                <DynamicGraph graph={graphData || null }/>
+                <DynamicGraph graph={graphData}/>
             </div>
 
         </Layout>
