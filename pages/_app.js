0038c565987faa14cd200179bd1803104659d6ae
Tuan Cao <caomanhtuanbsb@gmail.com>
2022-05-26 11:29:36 +0700

    Update tracking to mindstone.tuancao.me

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
-import '../styles/global.css'
-import '../styles/style.css'
-import "../styles/prism.css"
-
+import "../styles/global.css";
+import "../styles/style.css";
+import "../styles/prism.css";
+import PlausibleProvider from "next-plausible";
 export default function App({ Component, pageProps }) {
-
-  return <Component { ...pageProps} />
+  return (
+    <PlausibleProvider domain="mindstone.tuancao.me">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 }
